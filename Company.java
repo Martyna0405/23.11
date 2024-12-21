326102752259c538b21ce8a22c1a29419a7b9982
Martyna0405 <kotulska.martyna1@gmail.com>
2024-12-21 09:31:39 +0100

    Update Company.java

--- Company.java
+++ Company.java
@@ -1,2 +1,19 @@
 public class Company {
+  private String name;
+  private Address adress;
+
+  private List<Employee>employees;
+  
+  public Company(String name, Address address){
+    this.name = name;
+    this.address = address;
+    this.employees = new ArrayList<>();
+  }
+  public String getName() { return name;}
+  public void setName(String name) {this.name = name;}
+  public Address getAddress(){return address;}
+  public void setAddress(Address address) {this.address = address;}
+
+  public List<Emplolyee>getEmployyes(){
+    return employees;}
 }
