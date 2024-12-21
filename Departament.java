283f03efb411c90475d755bd7b86cc9f3ef84f1a
Martyna0405 <kotulska.martyna1@gmail.com>
2024-12-21 08:34:03 +0100

    Update Departament.java

--- Departament.java
+++ Departament.java
@@ -1,7 +1,10 @@
-package org.example;
+package Test; //Nazwa projektu//
 
 public enum Departament {
-    SALES("spprzedaż")
-    ADMINITRATION("administracja")
-        private String
+    SALES("spprzedaż");
+    ADMINITRATION("administracja");
+    FINANCE("Finanse");
+        private String name;
+    Departament(String name) {this.name = name;}
+    public String getName(){return name;}
 }
